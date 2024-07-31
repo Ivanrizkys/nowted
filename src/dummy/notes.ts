de00a4e8f131785e2f485b9043d7882fe6d2484a
Ivanrizkys <ivnrzkys@gmail.com>
2024-07-31 14:18:18 +0700

    chore: adding dummy data notes

---
+++ src/dummy/notes.ts
@@ -0,0 +1,53 @@
+export interface NotesType {
+	id: number;
+	title: string;
+	date: string;
+	headline: string;
+}
+
+const Notes: NotesType[] = [
+	{
+		id: 1,
+		title: "My Goals for the Next Year",
+		date: "31/12/2022",
+		headline: "Use this online playground to view and fork example",
+	},
+	{
+		id: 2,
+		title: "Reflection on the Month of June",
+		date: "31/12/2022",
+		headline: "Use this online playground to view and fork example",
+	},
+	{
+		id: 3,
+		title: "My Favorite Memories from Childhood",
+		date: "31/12/2022",
+		headline: "Use this online playground to view and fork example",
+	},
+	{
+		id: 4,
+		title: "Reflections on My First Year of College",
+		date: "31/12/2022",
+		headline: "Use this online playground to view and fork example",
+	},
+	{
+		id: 5,
+		title: "My Experience with Meditation",
+		date: "31/12/2022",
+		headline: "Use this online playground to view and fork example",
+	},
+	{
+		id: 6,
+		title: "Thoughts on the Pandemic",
+		date: "31/12/2022",
+		headline: "Use this online playground to view and fork example",
+	},
+	{
+		id: 7,
+		title: "My Favorite Recipes",
+		date: "31/12/2022",
+		headline: "Use this online playground to view and fork example",
+	},
+];
+
+export default Notes;

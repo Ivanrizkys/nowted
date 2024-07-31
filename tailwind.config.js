b01a8b933ffe213814c7ef17c2fce0632ab96413
Ivanrizkys <ivnrzkys@gmail.com>
2024-07-31 14:19:41 +0700

    chore: extends tailwind class and write load source sans pro font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -5,8 +7,18 @@ export default {
 		extend: {
 			fontFamily: {
 				"source-sans-pro": ["Source Sans Pro"],
+				"kaushan-script": ["Kaushan Script", "cursive"],
+			},
+			colors: {
+				"primary-100": "#1C1C1C",
+				"primary-200": "#181818",
+				"secondary-100": "#312EB5",
+				"tertiary-100": "#FFFFFF",
+			},
+			width: {
+				"radix-select-width": "var(--radix-select-trigger-width)",
 			},
 		},
 	},
-	plugins: [],
+	plugins: [tailwindAnimate],
 };

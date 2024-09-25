b0bc5c275741ad33d8451a3d47d4d3c0cc92f064
Ivanrizkys <ivnrzkys@gmail.com>
2024-09-25 20:48:27 +0700

    chore: wrap app with rxdb provider and google oAuth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Home from "@/components/pages/Home";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Provider as RxDbProvider } from "rxdb-hooks";
+import { useCreateRxDatabase } from "./config/rxdb";
 
 function App() {
 	return (
-		<>
-			<Home />
-		</>
+		<RxDbProvider db={useCreateRxDatabase()}>
+			<GoogleOAuthProvider
+				clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}
+			>
+				<Home />
+			</GoogleOAuthProvider>
+		</RxDbProvider>
 	);
 }
 

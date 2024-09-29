c8c21766de9592f34d8d9084c830f9cb633cd2f4
Ivanrizkys <ivnrzkys@gmail.com>
2024-09-29 17:05:05 +0700

    feat: adding opaciting animation in first render

--- src/components/ui/EmptyNote/index.tsx
+++ src/components/ui/EmptyNote/index.tsx
@@ -1,15 +1,24 @@
+import { animated, useSpring } from "@react-spring/web";
 import { FileText } from "lucide-react";
 
 const EmptyNote = () => {
+	const motion = useSpring({
+		from: { opacity: 0 },
+		to: { opacity: 1 },
+	});
+
 	return (
-		<div className="h-full flex flex-col items-center justify-center gap-4 text-center p-12">
+		<animated.div
+			style={{ ...motion }}
+			className="h-full flex flex-col items-center justify-center gap-4 text-center p-12"
+		>
 			<FileText className="w-[53px] h-[66px]" />
 			<h2 className="font-semibold text-3xl">Select a note to view</h2>
 			<p className="w-full max-w-[460px] text-tertiary-100/60">
 				Choose a note from the list on the left to view its contents, or create
 				a new note to add to your collection.
 			</p>
-		</div>
+		</animated.div>
 	);
 };
 
